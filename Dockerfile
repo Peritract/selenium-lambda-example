b64e110d3c19b8723c7021cc523566e929c1a2b2
Dan <peritract@hotmail.co.uk>
2023-05-18 23:33:53 +0100

    VICTORY

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,14 @@
 FROM public.ecr.aws/lambda/python:3.10
 WORKDIR ${LAMBDA_TASK_ROOT}
+RUN yum update -y \
+    && yum clean all \
+    && yum install -y tar gzip bzip2
+RUN yum install xz atk cups-libs gtk3 libXcomposite alsa-lib tar \
+    libXcursor libXdamage libXext libXi libXrandr libXScrnSaver \
+    libXtst pango at-spi2-atk libXt xorg-x11-server-Xvfb \
+    xorg-x11-xauth dbus-glib dbus-glib-devel unzip bzip2 -y -q
+COPY install-firefox.sh .
+RUN bash install-firefox.sh
 COPY requirements.txt  .
 RUN  pip3 install -r requirements.txt
 COPY app.py .
--- app.py
+++ app.py
@@ -1,9 +1,35 @@
-from os import environ as env
+from os import environ as env, mkdir
+import json
 
 from dotenv import load_dotenv
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver import Firefox, FirefoxProfile
+from selenium.webdriver.common.by import By
+
+def get_headline(url, driver):
+    driver.get(url)
+    return driver.find_element(By.TAG_NAME,'h1').text
+
 
 def handler(event, context):
-    return env.get("EXAMPLE")
+    firefox_options = Options()
+    firefox_options.add_argument("-headless")
+    firefox_options.binary_location = '/opt/firefox/113.0/firefox/firefox'
+    tmp_dir = '/tmp/ff' 
+    mkdir(tmp_dir)
+    ff_profile = FirefoxProfile(profile_directory=tmp_dir)
+    driver = Firefox(firefox_profile=ff_profile,
+                     executable_path='/opt/geckodriver/0.33.0/geckodriver',
+                     options=firefox_options,
+                     service_log_path='/tmp/geckodriver.log')
+
+    # Proof!
+    url = "https://www.bbc.co.uk/news/world-europe-45753455"
+    headline = get_headline(url, driver)
+
+    driver.close()
+
+    return headline
 
 if __name__ == "__main__":
     load_dotenv()
