2f35d6dba8e72b42d74550c1c736fc1188f2c4b8
Dan <peritract@hotmail.co.uk>
2023-05-24 17:38:30 +0100

    email test

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,5 @@
 FROM public.ecr.aws/lambda/python:3.10
 WORKDIR ${LAMBDA_TASK_ROOT}
-RUN yum update -y \
-    && yum clean all \
-    && yum install -y tar gzip bzip2
-RUN yum install xz atk cups-libs gtk3 libXcomposite alsa-lib \
-    libXcursor libXdamage libXext libXi libXrandr libXScrnSaver \
-    libXtst pango at-spi2-atk libXt xorg-x11-server-Xvfb \
-    xorg-x11-xauth dbus-glib dbus-glib-devel -y -q
-RUN rm -rf /var/cache/yum
-COPY install-firefox.sh .
-RUN bash install-firefox.sh
 COPY requirements.txt  .
 RUN  pip3 install -r requirements.txt
 COPY app.py .
--- app.py
+++ app.py
@@ -1,37 +1,75 @@
-from os import environ as env, mkdir
-import json
+from os import environ as env
+import base64
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 
 from dotenv import load_dotenv
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver import Firefox, FirefoxProfile
-from selenium.webdriver.common.by import By
+from xhtml2pdf import pisa
+import plotly_express as px
+import boto3
 
-def get_headline(url, driver):
-    driver.get(url)
-    return driver.find_element(By.TAG_NAME,'h1').text
+
+def convert_html_to_pdf(source_html, output_filename):
+    # open output file for writing (truncated binary)
+    result_file = open(output_filename, "w+b")
+
+    # convert HTML to PDF
+    pisa_status = pisa.CreatePDF(
+            source_html,                # the HTML to convert
+            dest=result_file)           # file handle to recieve result
+
+    # close output file
+    result_file.close()                 # close output file
+
+    # return True on success and False on errors
+    return pisa_status.err
+
+def create_report():
+    img_bytes = px.scatter([1, 2, 3], [4, 5, 6]).to_image()
+    img = base64.b64encode(img_bytes).decode("utf-8")
+    template = f'''
+    <h1>TMNT</h1>
+    <p>Example!</p>
+    <img style="width: 400; height: 600" src="data:image/png;base64,{img}">
+    '''
+
+    convert_html_to_pdf(template, env.get("REPORT_FILE"))
+
+
+def send_email():
+
+    client = boto3.client("ses",
+                          region_name="eu-west-2",
+                          aws_access_key_id=env["AWS_ACCESS_KEY_ID"],
+                          aws_secret_access_key=env["AWS_SECRET_ACCESS_KEY"])
+    message = MIMEMultipart()
+    message["Subject"] = "Local Test"
+
+    attachment = MIMEApplication(open(env.get("REPORT_FILE"), 'rb').read())
+    attachment.add_header('Content-Disposition', 'attachment', filename='report.pdf')
+    message.attach(attachment)
+
+    print(message)
+
+    client.send_raw_email(
+        Source='dan.keefe@sigmalabs.co.uk',
+        Destinations=[
+            'dan.keefe@sigmalabs.co.uk',
+        ],
+        RawMessage={
+            'Data': message.as_string()
+        }
+    )
+
 
 
 def handler(event, context):
-    firefox_options = Options()
-    firefox_options.add_argument("-headless")
-    firefox_options.binary_location = '/opt/firefox/113.0/firefox/firefox'
-    tmp_dir = '/tmp/ff' 
-    mkdir(tmp_dir)
-    ff_profile = FirefoxProfile(profile_directory=tmp_dir)
-    driver = Firefox(firefox_profile=ff_profile,
-                     executable_path='/opt/geckodriver/0.33.0/geckodriver',
-                     options=firefox_options,
-                     service_log_path='/tmp/geckodriver.log')
+    create_report()
+    print("Report created.")
+    send_email()
+    print("Email sent.")
 
-    # Proof!
-    url = "https://www.bbc.co.uk/news/world-europe-45753455"
-    headline = get_headline(url, driver)
-
-    driver.close()
-
-    return headline
 
 if __name__ == "__main__":
     load_dotenv()
-    print(env.get("EXAMPLE"))
     print(handler(None, None))
